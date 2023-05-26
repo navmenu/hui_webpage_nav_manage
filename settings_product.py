a1efebc9f0074b6ea99dd578204eb93db555ed1c
yile yang <375911733@qq.com>
2023-05-26 19:13:07 +0700

    2023-05-26 19:13:07

--- settings_product.py
+++ settings_product.py
@@ -7,5 +7,6 @@ print("settings_online")
 DEBUG = False  # 注意这里是新增了个变量但由于恰好重名因此覆盖了老变量，其最终会影响到 django.conf.settings.DEBUG 的真假
 LOGGING['root']['level'] = 'INFO'
 # ELASTIC_APM['DEBUG'] = False
+ALLOWED_HOSTS = ['*']
 
 print("---------------")
