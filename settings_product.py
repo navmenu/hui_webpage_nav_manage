6535a9cbf4172a0b2ee8a287ead4d23c798d293d
yile yang <375911733@qq.com>
2023-05-30 15:59:03 +0700

    2023-05-30 15:59:03

--- settings_product.py
+++ settings_product.py
@@ -1,4 +1,5 @@
 # use the normal settings
+from hui_webpage_nav_manage import settings
 from hui_webpage_nav_manage.settings import *
 
 print("---------------")
@@ -8,5 +9,8 @@ DEBUG = False  # 注意这里是新增了个变量但由于恰好重名因此覆
 LOGGING['root']['level'] = 'INFO'
 # ELASTIC_APM['DEBUG'] = False
 ALLOWED_HOSTS = ['*']
+
+BACKEND_API_PREFIX = "http://127.0.0.1:38000/api/web-navigation/v1/"
+BACKEND_ANONYMOUS_TOKEN = "abc"  # 在没有登陆时，还是需要个token和服务器交互的
 
 print("---------------")
